a057683c28bfd56686f5e09ec5c7307bfb112a7b
张磊 <zhanglei@zhangleidemac-mini.local>
2015-10-28 16:29:06 +0800

    Initial Commit

---
+++ Imaginarium/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Imaginarium
+//
+//  Created by 张磊 on 15/10/28.
+//  Copyright © 2015年 ZhengMu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Imaginarium/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Imaginarium
+//
+//  Created by 张磊 on 15/10/28.
+//  Copyright © 2015年 ZhengMu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
